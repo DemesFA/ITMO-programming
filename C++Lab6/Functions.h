c626867fadf819a7eaecf5a1f4e01deb0a8d1669
Rafael Murtazin <murtazin234@yandex.ru>
2023-05-18 08:34:36 +0300

    [MODIFY]

--- C++Lab6/Functions.h
+++ C++Lab6/Functions.h
@@ -1,4 +1,14 @@
 #include <functional>
 
-template <typename Iterator>
-bool none_of(Iterator begin,Iterator end, bool(*Predicate)(const auto)){}
+
+struct Functions{
+
+    template <typename Iterator,typename Predicate>
+    bool none_of(Iterator begin, Iterator end, Predicate predicate);
+
+    template <typename Iterator,typename Compare>
+    bool is_sorted(Iterator begin, Iterator end, Compare compare);
+
+    template<typename Iterator, typename T>
+    Iterator find_not(Iterator begin, Iterator end, T& value);
+};
--- C++Lab6/main.cpp
+++ C++Lab6/main.cpp
@@ -3,14 +3,25 @@
 #include <vector>
 #include <algorithm>
 
-using type = std::vector<int>::iterator::value_type
 
 int main() {
-    std::vector<int> vec;
 
-    int k;
-    cin >> k;
-    cout << k;
+    int Size;
+    std::cout << "Enter a size of vector" << std::endl;
+    std::cin >> Size;
+    std::vector<int> vec(Size);
+    std::cout << "Enter elements in vector" << std::endl;
+    for(int i = 0; i < vec.size(); i++){
+        std::cin >> vec[i];
+    }
 
-    return 0;
+    Functions functions;
+    bool noneOfResult = functions.none_of(vec.begin(),vec.end(), [](int num) {return num > 10; });
+    std::cout << "none_of Result: " << (noneOfResult ? "true" : "false") << std::endl;
+
+    bool isSortedResult = functions.is_sorted(vec.begin(), vec.end(), std::less<int>());
+    std::cout << "is_sorted Result: " << (isSortedResult ? "true" : "false") << std::endl;
+
+
+
 }
